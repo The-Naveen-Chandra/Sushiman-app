dd5f73c874b20c5f532171ea2884053fa0e63ae3
Naveen Chandra <the.naveenchandra@gmail.com>
2023-08-04 19:08:03 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sushiman_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sushiman_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/landing/landing_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
